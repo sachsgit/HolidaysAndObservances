6a45936151689654447b5455e118d36978fe39d7
Sachs <25045377+sachsgit@users.noreply.github.com>
2023-02-16 09:17:01 -0500

    Update holidayAndObservances.pl

--- holidayAndObservances.pl
+++ holidayAndObservances.pl
@@ -1,21 +1,23 @@
 #!/usr/bin/perl
 use strict;
+use utf8;
 use warnings;
-use Time::localtime;
 use HTML::TreeBuilder::XPath;
+use Text::Unidecode;
+use Time::localtime;
 
 my $tm = localtime;
 my ( $wday, $mday, $month, $year ) = ( $tm->wday, $tm->mday, $tm->mon, $tm->year + 1900 );
 $month = (
-    qw/ january   february  march
-      april     may       june
-      july      august    september
-      october   november  december  /
+	qw/ january   february  march
+    	april     may       june
+		july      august    september
+		october   november  december  /
 )[$month];
 $wday = (
-    qw/ Sunday Monday Tuesday
-    Wednesday Thursday Friday
-    Saturday /
+	qw/ Sunday Monday Tuesday
+    	Wednesday Thursday Friday
+    	Saturday /
 )[$wday];
 
 print "$wday, $mday " . capitalize($month) . " $year\n";
@@ -26,14 +28,14 @@ my $xpath = "//div[\@id='ContentColumn']/div/ul[1]/li";
 my $tree = HTML::TreeBuilder->new_from_url($url);
 my @entries = $tree->findvalues($xpath);
 foreach my $entry ( @entries ) {
-    print "*\t$entry\n";
+	print "*\t" . unidecode($entry) . "\n";
 }
 
 exit 0;
 
 sub capitalize {
-    my $word = shift;
-    $word =~ s/([a-z])(\w+)/\u$1\L$2/;
-    return $word;
+	my $word = shift;
+	$word =~ s/([a-z])(\w+)/\u$1\L$2/;
+	return $word;
 }
 __END__
